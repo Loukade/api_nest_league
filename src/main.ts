0a728cd58b41c136747cffc2005eba739b712366
LucasReckall <lucas.jenvrain@gmail.com>
2025-04-10 12:49:55 +0200

    [Update] Migrate from Express to Fastify for improved performance

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
