346ea79f3b85f9ccc24940c4d643663992659eb4
LucasReckall <lucas.jenvrain@gmail.com>
2025-04-11 16:00:43 +0200

    [Add] Cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  app.enableCors({
+    origin: '*', // Autoriser uniquement votre frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Méthodes autorisées
+    credentials: true, // Autoriser les cookies et les informations d'identification
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
